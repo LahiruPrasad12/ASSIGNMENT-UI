baf29bcdb7422bae76bb6a21d722334ec20b495e
LahiruPrasad12 <lahirupr471@gmail.com>
2022-08-08 14:49:12 +0530

    created nav bar for user

--- src/views/student-dashboard/home.jsx
+++ src/views/student-dashboard/home.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import StudentNav from '../../layouts/student-navbar'
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import {Button} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+
 
 const Home = () => {
     return (
-        <div>
-            Hello
+
+        <Container component="main" sx={{marginTop: '3%'}}>
             <StudentNav/>
-        </div>
+            <box
+                sx={{
+                    marginTop: 8,
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}>
+                <Button sx={{
+                    float: "right",
+                    marginTop:'20px'
+                }} variant="contained" startIcon={<AddIcon />}>Create Note</Button>
+
+            </box>
+        </Container>
     );
 };
 

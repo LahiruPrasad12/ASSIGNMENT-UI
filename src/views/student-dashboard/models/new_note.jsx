0b4f1a84b81e5b7c1302046ab910a1d763832301
LahiruPrasad12 <lahirupr471@gmail.com>
2022-08-08 16:13:47 +0530

    stylish add note compoennets

--- src/views/student-dashboard/models/new_note.jsx
+++ src/views/student-dashboard/models/new_note.jsx
@@ -1,52 +1,131 @@
-import React, {useState} from 'react';
-import {Button} from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogActions from "@mui/material/DialogActions";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
+import {styled} from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
+import AddIcon from '@mui/icons-material/Add';
+import TextField from "@mui/material/TextField";
+import {useFormik} from "formik";
+import {LoginForm} from "../../../validations";
+import {TextareaAutosize} from "@mui/material";
 
-function CreateNote() {
+
+const BootstrapDialog = styled(Dialog)(({theme}) => ({
+    '& .MuiDialogContent-root': {
+        padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+        padding: theme.spacing(1),
+    },
+}));
+
+const BootstrapDialogTitle = (props) => {
+    const {children, onClose, ...other} = props;
+
+    return (
+        <DialogTitle sx={{m: 0, p: 2}} {...other}>
+            {children}
+            {onClose ? (
+                <IconButton
+                    aria-label="close"
+                    onClick={onClose}
+                    sx={{
+                        position: 'absolute',
+                        right: 8,
+                        top: 8,
+                        color: (theme) => theme.palette.grey[500],
+                    }}
+                >
+                    <CloseIcon/>
+                </IconButton>
+            ) : null}
+        </DialogTitle>
+    );
+};
+
+BootstrapDialogTitle.propTypes = {
+    children: PropTypes.node,
+    onClose: PropTypes.func.isRequired,
+};
+
+export default function CustomizedDialogs() {
     const [open, setOpen] = React.useState(false);
 
     const handleClickOpen = () => {
         setOpen(true);
     };
-
     const handleClose = () => {
         setOpen(false);
     };
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: LoginForm,
+        onSubmit: (values) => {
+            // login(values);
+        },
+    });
+
     return (
         <div>
             <Button sx={{
                 float: "right",
-                marginTop:'20px'
-            }} variant="contained" startIcon={<AddIcon />} onClick={handleClickOpen}>Create Note</Button>
-            <Dialog
-                open={open}
+                marginTop: '20px'
+            }} variant="contained" startIcon={<AddIcon/>} onClick={handleClickOpen}>Create Note</Button>
+            <BootstrapDialog
                 onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
+                aria-labelledby="customized-dialog-title"
+                open={open}
             >
-                <DialogTitle id="alert-dialog-title">
-                    {"Use Google's location service?"}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Let Google help apps determine location. This means sending anonymous
-                        location data to Google, even when no apps are running.
-                    </DialogContentText>
+                <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
+                    CREATE NEW NOTE
+                </BootstrapDialogTitle>
+                <DialogContent dividers>
+                    <Typography gutterBottom>
+                        <TextField
+                            fullWidth
+                            margin="normal"
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            error={formik.touched.email && Boolean(formik.errors.email)}
+                            helperText={formik.touched.email && formik.errors.email}
+                        />
+                        <TextField
+                            fullWidth
+                            margin="normal"
+                            id="email"
+                            label="Description"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            error={formik.touched.email && Boolean(formik.errors.email)}
+                            helperText={formik.touched.email && formik.errors.email}
+
+                        />
+                    </Typography>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleClose}>Disagree</Button>
-                    <Button onClick={handleClose} autoFocus>
-                        Agree
+                    <Button autoFocus onClick={handleClose}>
+                        Save changes
                     </Button>
                 </DialogActions>
-            </Dialog>
+            </BootstrapDialog>
         </div>
     );
 }
-
-export default CreateNote;

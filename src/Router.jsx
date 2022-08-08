7d3f2bc6617d4c3cb53970c8f7b2efde8eb54fab
LahiruPrasad12 <lahirupr471@gmail.com>
2022-08-08 13:13:54 +0530

    created nav bar for student

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Auth_routes from './routes/auth_routes'
+import Student_routes from './routes/student_routes'
 const Routers = () => {
     return (
         <div>
             <Auth_routes/>
+            <Student_routes/>
         </div>
     );
 };
---
+++ src/views/student-dashboard/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import StudentNav from '../../layouts/student-navbar'
+
+const Home = () => {
+    return (
+        <div>
+            Hello
+            <StudentNav/>
+        </div>
+    );
+};
+
+export default Home;
